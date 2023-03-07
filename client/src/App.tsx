ebf8ccd67adda0c2993927dd72e3f9a584e30dea
2realzoo <lunalee00@gmail.com>
2023-03-07 16:23:39 +0900

    [FE][STYLES] 색상 정의, 라이브러리 다운

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,20 @@ import Main from './pages/Main';
 import Login from './pages/Login';
 import MyPage from './pages/MyPage';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div>
+        Main
+    </div>
   )
 }
 
