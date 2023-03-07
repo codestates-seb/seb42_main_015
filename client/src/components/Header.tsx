860e97759382fb7e49c97e6f94ea9ba788bfe89d
2realzoo <lunalee00@gmail.com>
2023-03-07 16:22:42 +0900

    [FE][STYLES] 색상 정의, 라이브러리 다운

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled  from 'styled-components'
+
+const HeaderContainer = styled.div`
+  border-bottom: 10px solid #312f2b
+`;
 
 function Header() {
   return (
-    <div>Header</div>
+    <HeaderContainer>Header</HeaderContainer>
   )
 }
 
