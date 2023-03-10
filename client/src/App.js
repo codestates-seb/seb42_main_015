3811994670e4b218c1c077fb0a83851945d59ea3
2realzoo <lunalee00@gmail.com>
2023-03-10 16:26:36 +0900

    [FE][CHORE] 개발환경 재구축

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import MyPage from "./pages/MyPage";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
