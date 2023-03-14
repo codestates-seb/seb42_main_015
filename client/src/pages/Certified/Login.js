97f0e6e368738357cef7b72f5719fe1e82eba6a8
haizel <115685195+haizellatte@users.noreply.github.com>
2023-03-14 21:07:11 +0900

    Feat/login  (#39)
    * [FE][FEAT]로그인페이지
    
    * [FE][FEAT]로그인페이지 화면 구현
    
    * [FE][FEAT] 로그인페이지 화면 구현 완료
    
    ---------
    
    Co-authored-by: haizellatte <YOUR_EMAIL>

--- client/src/pages/Certified/Login.js
+++ client/src/pages/Certified/Login.js
@@ -1,8 +1,10 @@
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import * as L from "./LoginFormStyled";
 import Google from "../../asset/구글.png";
 import Kakao from "../../asset/카카오.png";
-import * as L from "./LoginFormStyled";
+import img1 from "../../asset/해바라기.png";
+import img2 from "../../asset/하늘.png";
 
 function Login() {
   //handleSubmit을 가져옵니다.
@@ -14,47 +16,64 @@ function Login() {
   return (
     <>
       <L.Background>
-        <L.BackgroundYellow>
-          <L.Container>
-            <L.Form onSubmit={handleSubmit(onValid, onInvalid)}>
-              <li className="formLeft">
-                <ul className="login-form">
-                  <li className="loginText">Log in</li>
-                  <input
-                    {...register("id")}
-                    type="text"
-                    placeholder="email address"
-                  />
-                  <input
-                    {...register("password")}
-                    type="password"
-                    placeholder="Password"
-                  />
-                  <input className="btn" type="submit" value="Log in" />
-
-                  <div className="sub-form ">
-                    <Link to="/">
-                      <li>forget Password</li>
-                    </Link>
-                    <Link to="/signup">
-                      <li>sign up</li>
-                    </Link>
+        <L.Container>
+          <L.BackgroundYellow />
+          <L.Form onSubmit={handleSubmit(onValid, onInvalid)}>
+            <li className="formLeft">
+              <ul className="login-form">
+                <li className="loginText">Log in</li>
+                <input
+                  className="idInput"
+                  {...register("id")}
+                  type="text"
+                  placeholder="email address"
+                />
+                <input
+                  className="pwdInput"
+                  {...register("password")}
+                  type="password"
+                  placeholder="Password"
+                />
+                <input className="btn" type="submit" value="Log in" />
+                <div className="sub-form ">
+                  <Link to="/">
+                    <li>forget Password</li>
+                  </Link>
+                  <Link to="/signup">
+                    <li>sign up</li>
+                  </Link>
+                </div>
+                <div className="oauth-form">
+                  <div className="oauth-head">Log in With</div>
+                  <div className="oauth">
+                    <img src={Google} alt="Googole" />
+                    <img src={Kakao} alt="Kakao" />
                   </div>
-                  <div className="oauth-form">
-                    <div className="oauth-head">Log in With</div>
-                    <div className="oauth">
-                      <img src={Google} alt="Googole" />
-                      <img src={Kakao} alt="Googole" />
-                    </div>
-                  </div>
-                </ul>
-              </li>
-              <li className="formRight">
-                <ui className="welcome">welcome!</ui>
-              </li>
-            </L.Form>
-          </L.Container>
-        </L.BackgroundYellow>
+                </div>
+              </ul>
+            </li>
+            <li className="formRight">
+              <ui className="welcome">welcome!</ui>
+              <div className="imgWrapper">
+                <div className="section1">
+                  <img src={img1} alt="Sunflower"></img>
+                  <span className="box">sunflower</span>
+                </div>
+                <div className="section2">
+                  <img src={img2} alt="Sky"></img>
+                  <span className="box">cloud</span>
+                </div>
+              </div>
+              <div className="oauth-form">
+                <div className="oauth-head">Log in With</div>
+                <div className="oauth">
+                  <img src={Google} alt="Googole" />
+                  <img src={Kakao} alt="Kakao" />
+                </div>
+              </div>
+            </li>
+          </L.Form>
+        </L.Container>
       </L.Background>
     </>
   );
