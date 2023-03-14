ba00d15ea44ee5e855962f06083ae66bebb8762c
jiyoonchol <jiyoon276479@gmail.com>
2023-03-14 18:26:51 +0900

    [BE][FEAT] Message 로직 구현 1

--- server/messageapp/src/main/java/com/witchdelivery/messageapp/message/entity/Message.java
+++ server/messageapp/src/main/java/com/witchdelivery/messageapp/message/entity/Message.java
@@ -1,4 +1,27 @@
 package com.witchdelivery.messageapp.message.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;                              // 편지 식별번호
+
+
+    @Column(nullable = false, length = 7000)
+    private String content;                             // 편지 내용
+
+    @Column(nullable = false)
+    private Long password;                              // 편지 비밀번호
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();                   // 생성 시간
 }
