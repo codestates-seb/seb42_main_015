9c9c878cfbf6f3f520e7041c4435ad088dbea717
2realzoo <115965399+2realzoo@users.noreply.github.com>
2023-03-14 13:04:22 +0900

    Feat/fe (#34)
    * [FE][FEAT] css 폰트 변수
    
    * [FE][FEAT] 편지작성 페이지 기본 모양
    
    * [FE][FEAT] RounButton 공용 스타일 생성
    
    * [FEAT]헤더 아이콘 추가, footer 생성
    
    * [CHORE] 로그인, 회원가입 페이지 font 추가
    
    * [CHORE] 회원가입, 로그인페이지 asset image추가
    
    * [FE][FEAT] MyPage 생성
    
    * [FEAT][CHORE] iconify 설치경로 변경
    
    * [FEAT][CHORE] header size 조정
    
    ---------
    
    Co-authored-by: sienna0715 <5609373@naver.com>
    Co-authored-by: haizellatte <YOUR_EMAIL>
    Co-authored-by: Sienna <91577106+sienna0715@users.noreply.github.com>

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,11 +1,32 @@
 import React from "react";
-import styled from "styled-components";
+import * as M from "./MyPageStyled";
+import { BiEditAlt } from "react-icons/bi";
 
-const Title = styled.div`
-  font-family: "Cavert", cursive;
-`;
 function MyPage() {
-  return <Title>MyPage</Title>;
+  return (
+    <M.MyPageContainer>
+      <M.FlexWrapper1>
+        <M.GNBWrapper>
+          <M.GNBMenu>내정보</M.GNBMenu>
+          <M.GNBMenu>비밀번호 수정</M.GNBMenu>
+          <M.GNBMenu>휴지통</M.GNBMenu>
+        </M.GNBWrapper>
+        <M.UserInfoCard>
+          <M.UserImage></M.UserImage>
+          <M.UserInfoWrapper>
+            <M.UserName>김햄찌</M.UserName>
+            <M.UserAboutMe>
+              저는 김햄찌입니다요
+              <M.EditButton>
+                edit
+                <BiEditAlt></BiEditAlt>
+              </M.EditButton>
+            </M.UserAboutMe>
+          </M.UserInfoWrapper>
+        </M.UserInfoCard>
+      </M.FlexWrapper1>
+    </M.MyPageContainer>
+  );
 }
 
 export default MyPage;
--- client/src/style/color.js
+++ client/src/style/color.js
@@ -2,3 +2,12 @@ export const PALETTE_WRITELETTER = {
   background: "#d7e5df",
   text_primary: "#005897",
 };
+export const PALETTE_V1 = {
+  background: "#FCFBF4",
+  text_primary: "#312F2B",
+  yellow_button: "#FFE848",
+};
+export const PALETTE_MYPAGE = {
+  gradation_green: "#91F09B",
+  active_green: "#53C848",
+};
