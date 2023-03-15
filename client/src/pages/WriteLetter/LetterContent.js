74f1bf47a0b3332e37e28ef52e40eb4d565729aa
2realzoo <115965399+2realzoo@users.noreply.github.com>
2023-03-15 15:37:05 +0900

    Feat/writeletter (#45)
    * [FE][FEAT] css 폰트 변수
    
    * [FE][FEAT] 편지작성 페이지 기본 모양
    
    * [FE][FEAT] RounButton 공용 스타일 생성
    
    * [FEAT]헤더 아이콘 추가, footer 생성
    
    * [CHORE] 로그인, 회원가입 페이지 font 추가
    
    * [CHORE] 회원가입, 로그인페이지 asset image추가
    
    * [FE][FEAT] MyPage 생성
    
    * [FEAT][CHORE] iconify 설치경로 변경
    
    * [FE][FEAT] audioRecord 생성
    
    * [FE][FEAT] writeLetter 도움말
    
    * [FE][FEAT] writeletter 도움말
    
    ---------
    
    Co-authored-by: sienna0715 <5609373@naver.com>
    Co-authored-by: haizellatte <YOUR_EMAIL>
    Co-authored-by: Sienna <91577106+sienna0715@users.noreply.github.com>

--- client/src/pages/WriteLetter/LetterContent.js
+++ client/src/pages/WriteLetter/LetterContent.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as W from "./WriteStyled";
 
-function LetterContent() {
-  return <W.ContentContainer></W.ContentContainer>;
+function LetterContent({ openExplaination }) {
+  const weekday = ["일", "월", "화", "수", "목", "금", "토"];
+  const currentDate = `${new Date().getFullYear()}.${new Date().getMonth()}.${new Date().getDate()} ${
+    weekday[new Date().getDay()]
+  }`;
+  const [checked, setChecked] = useState(false);
+  const handleCheckBox = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <W.LetterBox>
+      <W.FlexWrapper1>
+        <W.NameInputWrapper>
+          To
+          <W.NameInput></W.NameInput>
+        </W.NameInputWrapper>
+        <W.Date>{currentDate}</W.Date>
+      </W.FlexWrapper1>
+      <W.SendMeWrapper>
+        <W.SendMeCheckBox
+          checked={checked}
+          onClick={handleCheckBox}></W.SendMeCheckBox>
+        <W.SendMeLabel>나에게보내기</W.SendMeLabel>
+        {openExplaination ? (
+          <W.BallonLeft id="ballon1">
+            나에게 편지를 작성할 수 있습니다.
+          </W.BallonLeft>
+        ) : (
+          <></>
+        )}
+      </W.SendMeWrapper>
+      <W.ContentTextarea></W.ContentTextarea>
+      <W.FromWrapper>
+        <W.NameInputWrapper>
+          From
+          <W.NameInput></W.NameInput>
+        </W.NameInputWrapper>
+      </W.FromWrapper>
+    </W.LetterBox>
+  );
 }
 
 export default LetterContent;
