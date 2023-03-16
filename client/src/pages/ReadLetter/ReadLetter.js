97be468bfd01a7f3b4e63c538e69a99829b08f34
haizel <115685195+haizellatte@users.noreply.github.com>
2023-03-16 19:51:05 +0900

    [FE][FIX] 병합 충돌 해결 (#69)
    * [FE][FEAT] seccetLetter.js 생성
    
    * [FE][FEAT] 편지조회 모달
    
    ---------
    
    Co-authored-by: haizellatte <YOUR_EMAIL>

--- client/src/pages/ReadLetter/ReadLetter.js
+++ client/src/pages/ReadLetter/ReadLetter.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Link } from "react-router-dom";
+import * as R from "./ReadStyled";
+import SecretLetter from "./SecetLetter";
 
 const ReadLetter = () => {
-  return <div></div>;
+  const [enterPassword, setEnterPassword] = useState(false);
+
+  return (
+    <>
+      {enterPassword ? (
+        <R.SWrapper theme="open">
+          <div>오픈</div>
+        </R.SWrapper>
+      ) : (
+        <SecretLetter />
+      )}
+    </>
+  );
 };
 
 export default ReadLetter;
--- client/src/pages/ReadLetter/ReadStyled.js
+++ client/src/pages/ReadLetter/ReadStyled.js
@@ -1,3 +1,77 @@
 import styled from "styled-components";
 import BREAKPOINTMOBILE from "../../breakpoint";
-import { FONT_STYLE_LOGIN, FONT_STYLE_LOGOUT } from "../../style/fontStyle";
+import { FONT_STYLE_READ, FONT_STYLE_LOGOUT } from "../../style/fontStyle";
+import pwd from "../../asset/pwd.png";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 87vh;
+  padding: 3rem 2rem;
+  overflow: hidden;
+`;
+
+export const SWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 87vh;
+  padding: 3rem 2rem;
+  overflow: hidden;
+  background: #fffb95;
+`;
+
+export const CheckContainer = styled.div`
+  display: flex;
+  background: #d7e5df;
+  padding: 5rem 5rem;
+  width: 50vw;
+  height: 30vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+  justify-content: space-evenly;
+  div {
+    display: flex;
+    ${FONT_STYLE_READ.title_20_medium}
+  }
+  input {
+    display: flex;
+    width: 23rem;
+    height: 2.3rem;
+    background-size: 1.5rem;
+    padding: 0rem 3rem;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 1px solid #000;
+    background-image: url("${pwd}");
+    background-color: initial;
+    background-repeat: no-repeat;
+    background-position: left;
+    background-size: 1.5rem;
+    @media screen and (max-width: ${BREAKPOINTMOBILE}px) {
+      background-size: 1.2rem;
+    }
+  }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    background-color: #fcfbf4;
+    font-family: "B612", sans-serif;
+    font-size: 0.8rem;
+    width: 7rem;
+    height: 2.1rem;
+    cursor: pointer;
+    @media screen and (max-width: ${BREAKPOINTMOBILE}px) {
+      margin-top: 2rem;
+      width: 10rem;
+      height: 3rem;
+      ${FONT_STYLE_LOGOUT.title_9_medium}
+    }
+  }
+`;
