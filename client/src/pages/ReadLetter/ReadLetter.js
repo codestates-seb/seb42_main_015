a49ab01fea6da37c0a2bc16c2eea818baf6a8aa6
haizellatte <YOUR_EMAIL>
2023-03-17 14:51:32 +0900

    [FE][FEAT] font-styled 추가

--- client/src/pages/ReadLetter/ReadLetter.js
+++ client/src/pages/ReadLetter/ReadLetter.js
@@ -7,7 +7,10 @@ import * as R from "./ReadStyled";
 import SecretLetter from "./SecetLetter";
 
 const ReadLetter = ({ isLogin }) => {
+  //비밀번호 쳤는지 안쳤는지
   const [enterPassword, setEnterPassword] = useState(false);
+  //보관했는지 안했는지
+  const [isKeeping, setIsKeeping] = useState(false);
 
   return (
     <>
@@ -18,11 +21,18 @@ const ReadLetter = ({ isLogin }) => {
               비밀번호
               <input placeholder="****" />
             </R.EnterSeret>
-            <R.Letterpaper></R.Letterpaper>
+            <R.Letterpaper>
+              <div className="top">
+                <div className="to">To. 김햄찌</div>
+              </div>
+              <div className="date">2023.03.17 금</div>
+              <div className="content">{R.LetterEx}</div>
+              <div className="from">From. 오디토</div>
+            </R.Letterpaper>
             <R.Buttons>
-              <button>이미지로 저장하기</button>
+              <button>이미지 저장</button>
               <button>보관하기</button>
-              <button>닫기</button>
+              {/* <button className="close">보관완료</button> */}
             </R.Buttons>
           </div>
         </R.Wrapper>
