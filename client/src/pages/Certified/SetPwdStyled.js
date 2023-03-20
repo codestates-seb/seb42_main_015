c5a350e8dee7a51339b7644edc378c0830b8e909
sienna0715 <5609373@naver.com>
2023-03-20 16:38:51 +0900

    [FE][FEAT] 비밀번호 변경 성공 페이지 작업 완료

--- client/src/pages/Certified/SetPwdStyled.js
+++ client/src/pages/Certified/SetPwdStyled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { FONT_STYLE_LOGIN, FONT_STYLE_V1 } from "../../style/fontStyle";
+import { PALETTE_V1 } from "../../style/color";
 
 export const SetPwdWrap = styled.div`
   width: 100%;
@@ -77,7 +78,7 @@ export const ErrorMsg = styled.div`
   ${FONT_STYLE_V1.body.body_8_light}
   color: red;
   text-align: center;
-`
+`;
 
 export const EmailLabel = styled.label`
   margin-top: 3rem;
@@ -151,4 +152,77 @@ export const Button = styled.button`
   height: 40px;
   background-color: transparent;
   border: 1px solid black;
-`
+`;
+
+export const SuccessContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  .suc1 {
+    bottom: 10rem;
+    right: 10rem;
+    transform: rotate(50deg);
+  }
+  .suc2 {
+    width: 20rem;
+    top: -3rem;
+    right: -3rem;
+    transform: rotate(40deg);
+  }
+  .suc3 {
+    top: -3rem; 
+    left: 10rem;
+    transform: rotate(110deg);
+  }
+  .suc4 {
+    bottom: 5rem; 
+    left: -1rem;
+    transform: rotate(-50deg);
+  }
+  .suc5 {
+    top: 7rem; 
+    left: -5rem;
+  }
+  .suc6 {
+    width: 25rem;
+    bottom: -10rem; 
+    left: 10rem;
+    transform: rotate(-20deg);
+  }
+  .suc7 {
+    width: 20rem;
+    bottom: -5rem; 
+    right: -5rem;
+    transform: rotate(30deg);
+  }
+  
+`;
+
+export const SuccessImg = styled.img`
+  position: absolute;
+  width: 15rem;
+`;
+
+export const SuccessTitle = styled.div`
+  ${FONT_STYLE_V1.title.title_30_medium}
+`;
+
+export const SuccessContent = styled.div`
+  ${FONT_STYLE_V1.body.body_15_light}
+  text-align: center;
+  line-height: 1.5;
+  margin-top: 2rem;
+  margin-bottom: 3.5rem;
+`;
+
+export const SuccessButton = styled.button`
+  ${FONT_STYLE_V1.body.body_12_light}
+  padding: 0.5rem 3rem;
+  background-color: ${PALETTE_V1.background};
+  border: 1px solid black;
+  cursor: pointer;
+`;
