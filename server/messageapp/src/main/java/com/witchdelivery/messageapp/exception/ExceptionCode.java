46bd51d74796033b6ff31cca4851faa3a74fb949
jiyoonchol <jiyoon276479@gmail.com>
2023-03-20 10:52:08 +0900

    [BE][FEAT] Message Password 구현 1

--- server/messageapp/src/main/java/com/witchdelivery/messageapp/exception/ExceptionCode.java
+++ server/messageapp/src/main/java/com/witchdelivery/messageapp/exception/ExceptionCode.java
@@ -9,7 +9,8 @@ public enum ExceptionCode {
     MEMBER_NAME_EXISTS(409, "MEMBER NAME EXISTS"),
     MESSAGE_NOT_FOUND(404, "MESSAGE NOT FOUND"),
     FILE_NOT_FOUND(404, "FILE NOT FOUND"),
-    FILE_SIZE_EXCEEDED(400, "FILE SIZE EXCEEDED");
+    FILE_SIZE_EXCEEDED(400, "FILE SIZE EXCEEDED"),
+    PASSWORD_NOT_MATCH(401, "PASSWORD NOT MATCH");
 
     private final int status;
     private  final String message;
