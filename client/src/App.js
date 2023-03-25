cbd476d90bc32f71368c72c31113fe0ab6398a7a
심효은 <simhyoeun@simhyoeun-ui-MacBookAir.local>
2023-03-25 21:37:02 +0900

    [FE][FEAT]readletter 태블릿사이즈 미디어쿼리 적용

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,12 @@ import PwdChange from "./pages/MyPage/PwdChange";
 import Trash from "./pages/MyPage/TrashList";
 import CompleteSignup from "./pages/Certified/CompleteSignup";
 import Preview from "./pages/WriteLetter/Preview";
-import { setCookie, getCookie } from "./pages/Certified/Cookie";
+import { getCookie } from "./pages/Certified/Cookie";
 import useStore from "./store/store";
-import axios from "axios";
 // ! 공백
 function App() {
   const displayFooter = true;
   const { isLogin, setIsLogin } = useStore((state) => state);
-  const { acessTokenExpire, setAcessTokenExpire } = useStore((state) => state);
 
   const initializeUserInfo = async () => {
     const loggedInfo = getCookie("accesstoken");
@@ -36,53 +34,6 @@ function App() {
   useEffect(() => {
     initializeUserInfo();
   }, [isLogin]);
-
-  //! Authorization(AccessToken) 재발급
-  //expire : Authorization : 1h // Refresh : 24h
-  const accessExpire =
-    new Date(getCookie("accesstoken_expire")).getTime() + 3600000;
-  const presentTime = new Date().getTime();
-
-  if (presentTime > accessExpire) {
-    setAcessTokenExpire(!acessTokenExpire);
-  }
-
-  const reIssueToken = async () => {
-    axios({
-      method: "post",
-      url: `/api/sendy/auth/reissue`,
-      headers: {
-        "ngrok-skip-browser-warning": "12",
-        Refresh: localStorage.getItem("refreshToken"),
-      },
-    })
-      .then((res) => {
-        if (res.headers.getAuthorization) {
-          //! accessToken -> cookie에 저장
-          setCookie(
-            "accesstoken",
-            `Bearer ${res.headers.get("Authorization").split(" ")[1]}`,
-            {
-              path: "/",
-              sucure: true,
-              sameSite: "Strict",
-              HttpOnly: " HttpOnly ",
-            }
-          );
-          //! accessToken expire  -> cookie에 저장(60분)
-          setCookie("accesstoken_expire", `${res.headers.get("Date")}`, {
-            options,
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    reIssueToken();
-  }, [acessTokenExpire]);
 
   // ! 공백
   return (
