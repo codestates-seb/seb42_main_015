64c333f2b7bd51fd59f50d229d2dfe5598cdb40b
haizellatte <YOUR_EMAIL>
2023-03-27 21:02:30 +0900

    [FE][FEAT]보관하기 기능 구현 완료

--- client/src/pages/ReadLetter/ReadButtons.js
+++ client/src/pages/ReadLetter/ReadButtons.js
@@ -1,149 +1,14 @@
-// import { Link, useParams } from "react-router-dom";
-// import * as R from "./ReadStyled";
-// import { PALETTE_V1 } from "../../style/color";
-// import ShadowButton from "../commons/ShadowButton";
-// import Modal from "../commons/Modal";
-// import LoginModal from "./LoginModal";
-// import { HiOutlineArrowUturnLeft, HiOutlineTrash } from "react-icons/hi2";
-// import axios from "axios";
-// import { getCookie } from "../Certified/Cookie";
-
-// const ReadButtons = ({
-//   isLogin,
-//   isKeeping,
-//   setIsKeeping,
-//   ModalRef,
-//   onDownloadBtn,
-// }) => {
-//   const { urlName } = useParams();
-
-//   //보관하기
-//   const handleKeeping = async () => {
-//     await axios({
-//       method: "patch",
-//       url: `/api/sendy/messages/saved/${urlName}`,
-//       headers: {
-//         "ngrok-skip-browser-warning": "12",
-//         Authorization: getCookie("accesstoken"),
-//       },
-//       body: {},
-//     })
-//       .then(() => {
-//         setIsKeeping(!isKeeping);
-//         window.location.reload();
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
-
-//   //! 휴지통 alert
-//   //outgoing 인지 receiving인지 우편함에서 상태 받아와야함 !
-//   const onRemove = () => {
-//     if (
-//       window.confirm(
-//         "정말로 삭제하시겠습니까?\n삭제된 편지는 [마이페이지-휴지통]에서 확인할 수 있습니다."
-//       )
-//     ) {
-//       alert("삭제되었습니다.");
-//     } else {
-//       return;
-//     }
-//   };
-
-//   return (
-//     <>
-//       <R.Buttons>
-//         {isLogin ? (
-//           <>
-//             <Link to="/letterbox">
-//               <HiOutlineArrowUturnLeft
-//                 size="30"
-//                 className="goback"
-//                 visibility="visible"
-//               />
-//             </Link>
-//             <HiOutlineTrash
-//               size="30"
-//               className="trash"
-//               onClick={onRemove}
-//               visibility="visible"
-//             />
-//           </>
-//         ) : (
-//           <>
-//             <HiOutlineArrowUturnLeft
-//               size="30"
-//               className="goback"
-//               visibility="hidden"
-//             />
-//             <HiOutlineTrash size="30" className="trash" visibility="hidden" />
-//           </>
-//         )}
-//         <ShadowButton
-//           className="button"
-//           backgroundColor={PALETTE_V1.yellow_button}
-//           state="none-block"
-//           onClick={onDownloadBtn}
-//         >
-//           이미지 저장
-//         </ShadowButton>
-//         {isLogin && isKeeping ? (
-//           <ShadowButton
-//             className="button"
-//             backgroundColor={PALETTE_V1.aready_keep_button}
-//             state="block"
-//           >
-//             보관완료
-//           </ShadowButton>
-//         ) : isKeeping ? (
-//           <ShadowButton
-//             className="button"
-//             backgroundColor={PALETTE_V1.yellow_button}
-//             onClick={handleKeeping}
-//             state="none-block"
-//           >
-//             보관하기
-//           </ShadowButton>
-//         ) : (
-//           <ShadowButton
-//             className="button"
-//             backgroundColor={PALETTE_V1.yellow_button}
-//             state="none-block"
-//             onClick={handleKeeping}
-//           >
-//             보관하기
-//           </ShadowButton>
-//         )}
-//         {}
-//         {isKeeping ? (
-//           <R.ModalBackground>
-//             <Modal
-//               ModalRef={ModalRef}
-//               ContainerHeight={"420px"}
-//               ContainerWidth={"370px"}
-//               children={
-//                 <LoginModal ModalRef={ModalRef} setIsKeeping={setIsKeeping} />
-//               }
-//             />
-//           </R.ModalBackground>
-//         ) : (
-//           <></>
-//         )}
-//       </R.Buttons>
-//     </>
-//   );
-// };
-
-// export default ReadButtons;
-
 import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 import * as R from "./ReadStyled";
 import { PALETTE_V1 } from "../../style/color";
 import ShadowButton from "../commons/ShadowButton";
 import Modal from "../commons/Modal";
 import LoginModal from "./LoginModal";
 import { HiOutlineArrowUturnLeft, HiOutlineTrash } from "react-icons/hi2";
+import axios from "axios";
+import { getCookie } from "../Certified/Cookie";
+import { Loading } from "../../components/Loading";
 
 const ReadButtons = ({
   isLogin,
@@ -155,10 +20,35 @@ const ReadButtons = ({
   setIsClickModal,
 }) => {
   const { urlName } = useParams();
+  //로딩상태
+  const [isLoading, setIsLoading] = useState(false);
 
   //'보관하기' 버튼 누르면 모달 나오는 이벤트 핸들러
-  const handleKeeping = () => {
+  const handleKeeping = async () => {
+    //모달 열기
     setIsClickModal(!isClickModal);
+    setIsLoading(true);
+    await axios({
+      method: "patch",
+      url: `/api/sendy/messages/saved/${urlName}`,
+      headers: {
+        "ngrok-skip-browser-warning": "12",
+        Authorization: getCookie("accesstoken"),
+      },
+      data: {},
+    })
+      .then(() => {
+        setIsLoading(false);
+        setIsKeeping(true);
+        setTimeout(() => {
+          alert("편지가 저장되었습니다.\n 이제 우편함에서 확인할 수 있어요!");
+        }, 100);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   };
 
   //! 휴지통 alert
@@ -178,8 +68,10 @@ const ReadButtons = ({
 
   return (
     <>
+      {isLoading ? <Loading /> : ""}
       <R.Buttons>
         {isLogin ? (
+          // 로그인 되어 있으면 -> 우편함, 휴지통 아이콘 보이게
           <>
             <Link to="/letterbox">
               <HiOutlineArrowUturnLeft
@@ -196,6 +88,7 @@ const ReadButtons = ({
             />
           </>
         ) : (
+          // 아니라면 hidden
           <>
             <Link to="/letterbox">
               <HiOutlineArrowUturnLeft
@@ -221,14 +114,29 @@ const ReadButtons = ({
           이미지 저장
         </ShadowButton>
         {isLogin ? (
-          <ShadowButton
-            className="button"
-            backgroundColor={PALETTE_V1.aready_keep_button}
-            state="block"
-          >
-            보관완료
-          </ShadowButton>
+          //로그인 되어 있다면 -> 저장 여부 확인
+          isKeeping ? (
+            // 저장되어 있다면 -> 보관완료
+            <ShadowButton
+              className="button"
+              backgroundColor={PALETTE_V1.aready_keep_button}
+              state="block"
+            >
+              보관완료
+            </ShadowButton>
+          ) : (
+            //저장 안되어 있다면 -> 보관하기
+            <ShadowButton
+              className="button"
+              backgroundColor={PALETTE_V1.yellow_button}
+              state="none-block"
+              onClick={handleKeeping}
+            >
+              보관하기
+            </ShadowButton>
+          )
         ) : (
+          //로그인 안되어 있다면
           <ShadowButton
             className="button"
             backgroundColor={PALETTE_V1.yellow_button}
@@ -238,7 +146,7 @@ const ReadButtons = ({
             보관하기
           </ShadowButton>
         )}
-        {isClickModal ? (
+        {isClickModal && !isLogin ? (
           <R.ModalBackground>
             <Modal
               ModalRef={ModalRef}
