90d88e4282d61ec780d22a13a055f6ae4b077119
yoonseonjin <xeccior@gmail.com>
2023-03-29 03:51:11 +0900

    [BE][CHORE] 자잘한 수정

--- server/messageapp/src/main/java/com/witchdelivery/messageapp/infra/email/EmailController.java
+++ server/messageapp/src/main/java/com/witchdelivery/messageapp/infra/email/EmailController.java
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
     private final EmailService emailService;
 
+    /**
+     * 랜덤 인증코드 이메일 발송 API
+     * @param emailPostDto
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/send-code-email")
     public ResponseEntity postCodeMail(@RequestBody EmailPostDto emailPostDto) throws Exception {
         EmailInfo emailInfo = EmailInfo.builder()
