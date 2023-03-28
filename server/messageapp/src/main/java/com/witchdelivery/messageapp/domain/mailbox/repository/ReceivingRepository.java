22502c62b0ae7d3f442d1f1f239ead1a7c818057
U-Lim <ylkk329@gmail.com>
2023-03-28 22:03:58 +0900

    [BE][FEAT] 휴지통 삭제 Batch

--- server/messageapp/src/main/java/com/witchdelivery/messageapp/domain/mailbox/repository/ReceivingRepository.java
+++ server/messageapp/src/main/java/com/witchdelivery/messageapp/domain/mailbox/repository/ReceivingRepository.java
@@ -5,10 +5,17 @@ import com.witchdelivery.messageapp.domain.mailbox.entity.Receiving;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReceivingRepository extends JpaRepository<Receiving, Long> { // 수신(받는 사람)
 
       Page<Receiving> findAllByMember_MemberIdAndReceivingStatusOrderByCreatedAtDesc(Long memberId, Receiving.ReceivingStatus receivingStatus, Pageable pageable);
 
       Page<Receiving> findAllByMember_MemberIdAndReceivingStatusOrderByDeletedAtDesc(Long memberId, Receiving.ReceivingStatus receivingStatus, Pageable pageable); // 휴지통 페이지네이션
+
+      //  deleted_at 날짜가 오늘 날짜로부터 30일보다 오래된 모든 메시지를 선택. 즉, 30일 이상 전에 생성된 메시지만 쿼리에서 반환된다.
+      @Query(value = "SELECT * FROM receiving WHERE DATE(deleted_at) < DATE_SUB(NOW(), INTERVAL 30 DAY)", nativeQuery = true)  // 배치
+      List<Receiving> selectLimitedReceiving();
 }
--- server/messageapp/src/main/java/com/witchdelivery/messageapp/global/config/BatchConfig.java
+++ server/messageapp/src/main/java/com/witchdelivery/messageapp/global/config/BatchConfig.java
@@ -1,5 +1,11 @@
 package com.witchdelivery.messageapp.global.config;
 
+import com.witchdelivery.messageapp.domain.mailbox.entity.Outgoing;
+import com.witchdelivery.messageapp.domain.mailbox.entity.Receiving;
+import com.witchdelivery.messageapp.domain.mailbox.repository.OutgoingRepository;
+import com.witchdelivery.messageapp.domain.mailbox.repository.ReceivingRepository;
+import com.witchdelivery.messageapp.domain.mailbox.service.DustbinService;
+import com.witchdelivery.messageapp.domain.mailbox.service.OutgoingService;
 import com.witchdelivery.messageapp.domain.message.entity.Message;
 import com.witchdelivery.messageapp.domain.message.repository.MessageRepository;
 import com.witchdelivery.messageapp.domain.message.service.MessageService;
@@ -29,6 +35,12 @@ public class BatchConfig {   // created_at이 7일 이상이고 messageSaved가 
     private MessageRepository messageRepository;
     @Autowired
     private MessageService messageService;
+    @Autowired
+    private OutgoingRepository outgoingRepository;
+    @Autowired
+    private ReceivingRepository receivingRepository;
+    @Autowired
+    private DustbinService dustbinService;
 
     @Bean
     public Job job() {
@@ -38,6 +50,26 @@ public class BatchConfig {   // created_at이 7일 이상이고 messageSaved가 
                 .build();
 
         return job;
+    }
+
+    @Bean
+    public Job outgoingJob() {
+
+        Job outgoingJob = jobBuilderFactory.get("outgoingJob")
+                .start(outgoingStep())
+                .build();
+
+        return outgoingJob;
+    }
+
+    @Bean
+    public Job receivingJob() {
+
+        Job receivingJob = jobBuilderFactory.get("receivingJob")
+                .start(receivingStep())
+                .build();
+
+        return receivingJob;
     }
 
     private Step step1() {
@@ -58,6 +90,46 @@ public class BatchConfig {   // created_at이 7일 이상이고 messageSaved가 
                 })
                 )
                 .build();         // Step 객체를 생성하기 위해 호출된다. Step 객체는 step1() 메서드에 의해 반환되고 Spring Batch 작업에 의해 실행된다.
+    }
+
+    private Step outgoingStep() {
+        return stepBuilderFactory.get("outgoingStep")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info("outgoingStep");
+
+                    List<Outgoing> limitedOutgoings = outgoingRepository.selectLimitedOutgoing();
+
+                    if(limitedOutgoings != null && !limitedOutgoings.isEmpty()) { // null이 아니거나 비어 있지 않은지 확인 (DB에 일치하는 값이 없는 경우에 NullPointerException 방지)
+                        for (Outgoing outgoing : limitedOutgoings) {
+                            if (outgoing.getOutgoingStatus()== Outgoing.OutgoingStatus.OUTGOING_DELETE) { // outgoingStatus가 Delete이면 삭제
+                                dustbinService.batchDeleteOutgoing(outgoing.getOutgoingId());
+                            }
+                        }
+                    }
+                    return RepeatStatus.FINISHED; // 반환, 생략 시 무한반복됨
+                })
+                )
+                .build(); // Step 객체를 생성하기 위해 호출된다. Step 객체는 outgoingStep() 메서드에 의해 반환되고 Spring Batch 작업에 의해 실행된다.
+    }
+
+    private Step receivingStep() {
+        return stepBuilderFactory.get("receivingStep")
+                .tasklet(((contribution, chunkContext) -> {
+                            log.info("receivingStep");
+
+                            List<Receiving> limitedReceivings = receivingRepository.selectLimitedReceiving();
+
+                            if(limitedReceivings != null && !limitedReceivings.isEmpty()) { // null이 아니거나 비어 있지 않은지 확인 (DB에 일치하는 값이 없는 경우에 NullPointerException 방지)
+                                for (Receiving receiving : limitedReceivings) {
+                                    if (receiving.getReceivingStatus()== Receiving.ReceivingStatus.RECEIVING_DELETE) { // receivingStatus가 Delete이면 삭제
+                                        dustbinService.batchDeleteReceiving(receiving.getReceivingId());
+                                    }
+                                }
+                            }
+                            return RepeatStatus.FINISHED; // 반환, 생략 시 무한반복됨
+                        })
+                )
+                .build(); // Step 객체를 생성하기 위해 호출된다. Step 객체는 receivingStep() 메서드에 의해 반환되고 Spring Batch 작업에 의해 실행된다.
     }
 
 }
