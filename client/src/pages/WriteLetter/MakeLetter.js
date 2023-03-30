4aa02e60468a6c3f814f353745e4993d6bce7f3c
2realzoo <lunalee00@gmail.com>
2023-03-30 21:47:20 +0900

    [FE][FEAT] 편지작성 리팩토링, 갤럭시 폴드 화면 맞춤

--- client/src/pages/WriteLetter/MakeLetter.js
+++ client/src/pages/WriteLetter/MakeLetter.js
@@ -5,8 +5,6 @@ import RoundButton from "../commons/RoundButton";
 import ShadowButton from "../commons/ShadowButton";
 import { FONT_STYLE_V1 } from "../../style/fontStyle";
 import { PALETTE_V1 } from "../../style/color";
-import addImage from "../../asset/add-image.png";
-import { BiX } from "react-icons/bi";
 import useStore from "../../store/store";
 import { useForm } from "react-hook-form";
 import { formSchema } from "./formSchema";
@@ -19,6 +17,7 @@ import {
   postMessage,
   postMessageImage,
 } from "../commons/axios";
+import ImageInput from "./ImageInput";
 
 function MakeLetter({ makeLetterModalRef }) {
   const {
@@ -26,84 +25,10 @@ function MakeLetter({ makeLetterModalRef }) {
     watch,
     formState: { errors, isValid },
   } = useForm({ mode: "onChange", resolver: yupResolver(formSchema) });
-  const [dragOver, setDragOver] = useState(false);
-  const [hasFile, setHasFile] = useState(false);
   const [image, setImage] = useState(null);
   const { letterContents, setLetterContents } = useStore();
   const [imageFile, setImageFile] = useState();
-  const renderFile = (file) => {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImage(reader.result);
-      setHasFile(true);
-    };
-  };
-  const checkFileSize = (file) => {
-    let maxSize = 4 * 1024 * 1024;
-    let fileSize = file.size;
-    if (fileSize > maxSize) {
-      alert("첨부파일 사이즈는 4MB 이내로 등록 가능합니다.");
-      return false;
-    }
-    return true;
-  };
-  const handleDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragOver(false);
-    if (e.dataTransfer.items) {
-      if (e.dataTransfer.items.length > 1) {
-        alert("사진은 하나만 업로드 가능합니다.");
-        return;
-      } else if (
-        e.dataTransfer.items[0].getAsFile().type !== "image/jpeg" &&
-        e.dataTransfer.items[0].getAsFile().type !== "image/png" &&
-        e.dataTransfer.items[0].getAsFile().type !== "image/gif"
-      ) {
-        alert("이미지 파일만 업로드 가능합니다.");
-        return;
-      } else if (checkFileSize(e.dataTransfer.items[0].getAsFile())) {
-        renderFile(e.dataTransfer.items[0].getAsFile());
-        setImageFile(e.dataTransfer.items[0].getAsFile());
-      }
-    } else {
-      if (e.dataTransfer.files.length > 1) {
-        alert("사진은 하나만 업로드 가능합니다.");
-        return;
-      } else if (
-        e.dataTransfer.files[0].type !== "image/png" &&
-        e.dataTransfer.files[0].type !== "image/jpeg" &&
-        e.dataTransfer.files[0].type !== "image/gif"
-      ) {
-        alert("이미지 파일만 업로드 가능합니다.");
-        return;
-      } else if (checkFileSize(e.dataTransfer.files[0])) {
-        renderFile(e.dataTransfer.files[0]);
-        setImageFile(e.dataTransfer.files[0]);
-      }
-    }
-  };
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragOver(true);
-  };
-  const handleDragleave = (e) => {
-    setDragOver(false);
-  };
-  const handleFile = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      if (checkFileSize(e.target.files[0])) {
-        renderFile(e.target.files[0]);
-        setImageFile(e.target.files[0]);
-      }
-    }
-  };
-  const handleDeleteFlie = (e) => {
-    setHasFile(false);
-    setImage(null);
-  };
+
   const [canUseUrl, setCanUseUrl] = useState(null);
   const handleCheckUrlName = () => {
     getUrlNameExist(letterContents.urlName)
@@ -156,6 +81,7 @@ function MakeLetter({ makeLetterModalRef }) {
     );
     window.open("/writeletter/preview");
   };
+
   const handleUrlReg = (e) => {
     setCanUseUrl(null);
     e.target.value = e.target.value.replace(
@@ -253,52 +179,12 @@ function MakeLetter({ makeLetterModalRef }) {
           )}
         </div>
       </W.FlexColunmWrapper>
-      <div>
-        <W.FlexRowWrapper className="align-items">
-          <W.Label>뒷면 사진 업로드</W.Label>
-          <p id="necessity">(선택)</p>
-        </W.FlexRowWrapper>
-        <W.UploadBox>
-          <div
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragleave}
-            className={dragOver ? "drag-file high-light" : "drag-file"}>
-            <W.FlexRowWrapper className="upload-box">
-              <W.FlexColunmWrapper className="align-center">
-                <img src={addImage} alt="파일 아이콘" className="image" />
-                <p className="message">업로드할 파일 끌어놓기</p>
-              </W.FlexColunmWrapper>
-
-              {hasFile ? (
-                <div className="preview-container" draggable>
-                  <img src={image} alt="preview" className="preview" />
-                  <div className="icon-container" onClick={handleDeleteFlie}>
-                    <BiX className="x-icon" />
-                  </div>
-                </div>
-              ) : (
-                <></>
-              )}
-            </W.FlexRowWrapper>
-          </div>
-          <label
-            onChange={handleFile}
-            className="file-label"
-            htmlFor="chooseFile">
-            파일 선택
-          </label>
-          <input
-            className="file"
-            id="chooseFile"
-            type="file"
-            onChange={handleFile}
-            accept="image/png, image/jpeg, image/gif"
-            multiple={false}
-          />
-        </W.UploadBox>
-      </div>
-
+      <ImageInput
+        image={image}
+        setImage={setImage}
+        imageFile={imageFile}
+        setImageFile={setImageFile}
+      />
       <W.FlexRowWrapper className="button-wrapper">
         <ShadowButton
           onClick={handlePreview}
